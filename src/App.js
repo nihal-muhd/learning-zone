ae92594fb16a24efcd5e872e851198cde739ff30
Nihal Muhammed <nihalmuhammed7025@gmail.com>
2026-07-21 21:57:43 +0530

    login and route protection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login';
+import { ProtectedRoute } from './routes/ProtectedRoute';
+import Dashboard from './pages/Dashboard';
+import { PublicRoute } from './routes/PublicRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route element={<PublicRoute />}>
+        <Route path='/login' element={<Login />} />
+
+      </Route>
+      <Route element={<ProtectedRoute />}>
+
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/' element={<Dashboard />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const VALID_EMAIL = "test@gmail.com";
+const VALID_PASSWORD = "123";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const hanldeSubmit = (event) => {
+    event.preventDefault();
+
+    const validUser =
+      VALID_EMAIL === formData.email && VALID_PASSWORD === formData.password;
+
+    if (!validUser) return;
+
+    localStorage.setItem("token", "fakeToken");
+    navigate("/dashboard", { replace: true });
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={hanldeSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+            name="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            value={formData.password}
+            onChange={handleChange}
+            name="password"
+          />
+        </div>
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
